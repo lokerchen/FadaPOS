66220cae3fd5218d536ee97cfb2aaba886606aa7
lokerchen <150758898@qq.com>
2020-09-24 23:55:14 +0800

    [+]增加FreeItem窗口

---
+++ SuperPOS/UI/TA/FrmFreeItem.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SuperPOS.UI.TA
+{
+    partial class FrmFreeItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new DevExpress.XtraEditors.SimpleButton();
+            this.btn2 = new DevExpress.XtraEditors.SimpleButton();
+            this.btn4 = new DevExpress.XtraEditors.SimpleButton();
+            this.btn3 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            this.btnOK = new DevExpress.XtraEditors.SimpleButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnOK);
+            this.groupBox1.Controls.Add(this.btn4);
+            this.groupBox1.Controls.Add(this.btn3);
+            this.groupBox1.Controls.Add(this.btn2);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(542, 340);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Free Items";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.label1.Location = new System.Drawing.Point(22, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(495, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer eligible for the free items below";
+            // 
+            // btn1
+            // 
+            this.btn1.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn1.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.btn1.Appearance.Options.UseBackColor = true;
+            this.btn1.Appearance.Options.UseFont = true;
+            this.btn1.Appearance.Options.UseForeColor = true;
+            this.btn1.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btn1.Location = new System.Drawing.Point(24, 93);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(242, 70);
+            this.btn1.TabIndex = 4;
+            // 
+            // btn2
+            // 
+            this.btn2.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn2.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.btn2.Appearance.Options.UseBackColor = true;
+            this.btn2.Appearance.Options.UseFont = true;
+            this.btn2.Appearance.Options.UseForeColor = true;
+            this.btn2.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btn2.Location = new System.Drawing.Point(272, 93);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(242, 70);
+            this.btn2.TabIndex = 5;
+            // 
+            // btn4
+            // 
+            this.btn4.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn4.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.btn4.Appearance.Options.UseBackColor = true;
+            this.btn4.Appearance.Options.UseFont = true;
+            this.btn4.Appearance.Options.UseForeColor = true;
+            this.btn4.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btn4.Location = new System.Drawing.Point(272, 170);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(242, 70);
+            this.btn4.TabIndex = 7;
+            // 
+            // btn3
+            // 
+            this.btn3.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn3.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Appearance.ForeColor = System.Drawing.Color.Black;
+            this.btn3.Appearance.Options.UseBackColor = true;
+            this.btn3.Appearance.Options.UseFont = true;
+            this.btn3.Appearance.Options.UseForeColor = true;
+            this.btn3.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btn3.Location = new System.Drawing.Point(24, 170);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(242, 70);
+            this.btn3.TabIndex = 6;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCancel.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Appearance.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Appearance.Options.UseBackColor = true;
+            this.btnCancel.Appearance.Options.UseFont = true;
+            this.btnCancel.Appearance.Options.UseForeColor = true;
+            this.btnCancel.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btnCancel.Location = new System.Drawing.Point(272, 255);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(242, 70);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnOK.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Appearance.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Appearance.Options.UseBackColor = true;
+            this.btnOK.Appearance.Options.UseFont = true;
+            this.btnOK.Appearance.Options.UseForeColor = true;
+            this.btnOK.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.btnOK.Location = new System.Drawing.Point(24, 255);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(242, 70);
+            this.btnOK.TabIndex = 8;
+            this.btnOK.Text = "OK";
+            // 
+            // FrmFreeItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 353);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmFreeItem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "FrmFreeItem";
+            this.Load += new System.EventHandler(this.FrmFreeItem_Load);
+            this.SizeChanged += new System.EventHandler(this.FrmFreeItem_SizeChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton btnCancel;
+        private DevExpress.XtraEditors.SimpleButton btnOK;
+        private DevExpress.XtraEditors.SimpleButton btn4;
+        private DevExpress.XtraEditors.SimpleButton btn3;
+        private DevExpress.XtraEditors.SimpleButton btn2;
+        private DevExpress.XtraEditors.SimpleButton btn1;
+    }
+}
