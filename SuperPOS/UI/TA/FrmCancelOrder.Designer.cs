90fcac839422d7d00222a4eabd6b29cb8a196ff5
Loker <150758898@qq.com>
2021-05-23 11:17:44 +0800

    [*]UI界面优化

--- SuperPOS/UI/TA/FrmCancelOrder.Designer.cs
+++ SuperPOS/UI/TA/FrmCancelOrder.Designer.cs
@@ -40,23 +40,22 @@
             // 
             this.label2.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(50, 118);
+            this.label2.Location = new System.Drawing.Point(44, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(361, 87);
+            this.label2.Size = new System.Drawing.Size(316, 68);
             this.label2.TabIndex = 1;
             this.label2.Text = "Are you sure you want to cancel the order?";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnYes
             // 
-            this.btnYes.BackColor = System.Drawing.Color.Gold;
+            this.btnYes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.btnYes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnYes.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
-            this.btnYes.ForeColor = System.Drawing.Color.Black;
-            this.btnYes.Location = new System.Drawing.Point(71, 278);
-            this.btnYes.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnYes.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold);
+            this.btnYes.ForeColor = System.Drawing.Color.White;
+            this.btnYes.Location = new System.Drawing.Point(62, 216);
             this.btnYes.Name = "btnYes";
-            this.btnYes.Size = new System.Drawing.Size(117, 57);
+            this.btnYes.Size = new System.Drawing.Size(102, 44);
             this.btnYes.TabIndex = 19;
             this.btnYes.Text = "Yes";
             this.btnYes.UseVisualStyleBackColor = false;
@@ -64,14 +63,13 @@
             // 
             // btnNo
             // 
-            this.btnNo.BackColor = System.Drawing.Color.Gold;
+            this.btnNo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNo.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
-            this.btnNo.ForeColor = System.Drawing.Color.Black;
-            this.btnNo.Location = new System.Drawing.Point(262, 278);
-            this.btnNo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnNo.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold);
+            this.btnNo.ForeColor = System.Drawing.Color.White;
+            this.btnNo.Location = new System.Drawing.Point(229, 216);
             this.btnNo.Name = "btnNo";
-            this.btnNo.Size = new System.Drawing.Size(117, 57);
+            this.btnNo.Size = new System.Drawing.Size(102, 44);
             this.btnNo.TabIndex = 20;
             this.btnNo.Text = "No";
             this.btnNo.UseVisualStyleBackColor = false;
@@ -81,9 +79,10 @@
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(-23, -4);
+            this.panel1.Location = new System.Drawing.Point(-20, -3);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(626, 49);
+            this.panel1.Size = new System.Drawing.Size(548, 39);
             this.panel1.TabIndex = 21;
             // 
             // label1
@@ -91,9 +90,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 12F);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(34, 14);
+            this.label1.Location = new System.Drawing.Point(30, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 24);
+            this.label1.Size = new System.Drawing.Size(101, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "Cancel Order";
             // 
@@ -101,14 +100,15 @@
             // 
             this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.Appearance.Options.UseBackColor = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(465, 387);
+            this.ClientSize = new System.Drawing.Size(407, 301);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnNo);
             this.Controls.Add(this.btnYes);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FrmCancelOrder";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "FrmCancelOrder";
